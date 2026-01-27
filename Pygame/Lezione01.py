53c3046b277703a0d1dd25e7c70b4b14047c2a79
nicvac <nicola.vacca@gmail.com>
2026-01-27 14:31:21 +0100

    pygame es 1

--- Pygame/Lezione01.py
+++ Pygame/Lezione01.py
@@ -1,13 +1,13 @@
 import pygame
+import sys
 
 pygame.init()
-
-screen = pygame.display.set_mode( (640,480) )
-
+screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Test Pygame")
 
-x = 320
-y = 240
+clock = pygame.time.Clock()
+
+x, y = 320, 240
 speed = 5
 
 running = True
@@ -15,19 +15,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     keys = pygame.key.get_pressed()
-
     if keys[pygame.K_LEFT]:
         x -= speed
-    elif keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]:
         x += speed
-    elif keys[pygame.K_UP]:
+    if keys[pygame.K_UP]:
         y -= speed
-    elif keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN]:
         y += speed
-
-    screen.fill( (30, 30, 50) )
-    pygame.draw.circle( screen, (255,100,100), (x, y), 30 )
-
+    
+    screen.fill((30, 30, 50))
+    pygame.draw.circle(screen, (255, 100, 100), (x, y), 30)
     pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
